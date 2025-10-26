d4c2cb1685457bab422825c44428d1d146548624
Diogo Garbinato <diogogarbinato@gmail.com>
2025-10-26 20:41:12 -0300

    feat: app style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import StartPage from "./components/StartPage";
 import GamePage from "./components/GamePage";
+import Blob1 from "./images/blob1.svg";
+import Blob2 from "./images/blob2.svg";
 
 function App() {
     const [hasGameStarted, setHasGameStarted] = useState(false);
 
     return (
         <main>
+            <img className="blob1" src={Blob1} />
             {!hasGameStarted ? <StartPage /> : <GamePage />}
+            <img className="blob2" src={Blob2} />
         </main>
     );
 }
